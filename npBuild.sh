f9d449a2691dd165abcba351f944d5d5d843887e
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2022-11-02 01:28:34 +0200

    Reorder functions.

--- npBuild.sh
+++ npBuild.sh
@@ -184,6 +184,134 @@ apply_finishing_touches() {
 
 # =========================== STANDALONE FUNCTIONS ===========================
 
+symlink_dotfiles() {
+    source "$HOME/.dotfiles/.zshenv"
+
+    [[ ! -d "$HOME/.config" ]] && mkdir -p "$HOME/.config"
+
+    [[ ! -d "$XDG_DATA_HOME/applications" ]] && mkdir -p "$XDG_DATA_HOME/applications"
+
+    # $HOME
+    for full_path in "$DOTFILES_DIR"/.[a-zA-Z]*; do
+        file="$(basename "$full_path")"
+        # Whenever you iterate over files/folders by globbing, it's good practice to avoid the corner 
+        # case where the glob does not match (which makes the loop variable expand to the 
+        # (un-matching) glob pattern string itself), hence [[ -e "$full_path" ] is used
+        [[ -e "$full_path" ]] && 
+            [[ "$file" != ".config" ]] &&
+            [[ "$file" != ".local" ]] &&
+            [[ "$file" != .git* ]] &&
+        ln -sf "$full_path" "$HOME/$file"
+    done
+
+    # $XDG_CONFIG_HOME
+    for full_path in "$DOTFILES_DIR/.config"/[a-zA-Z]*; do
+        file="$(basename "$full_path")"
+        [[ -e "$full_path" ]] &&
+        ln -sf "$full_path" "$XDG_CONFIG_HOME/$file"
+    done
+
+    # ~/.local/bin
+    local_bin="$DOTFILES_DIR/.local/bin"
+    [[ -e "$local_bin" ]] && ln -sf "$local_bin" "$HOME/.local/bin"
+
+    # ~/.local/share ($XDG_DATA_HOME)
+    local_shared="$DOTFILES_DIR/.local/share/riddle00"
+    [[ -e "$local_shared" ]] && ln -sf "$local_shared" "$XDG_DATA_HOME/riddle00"
+
+    for full_path in "$DOTFILES_DIR/.local/share/applications"/[a-zA-Z]*; do
+        file="$(basename "$full_path")"
+        [[ -e "$full_path" ]] &&
+        ln -sf $full_path "$XDG_DATA_HOME/applications/$file"
+    done
+}
+
+unlink_dotfiles() {
+    source "$HOME/.dotfiles/.zshenv"
+
+    # $HOME
+    for full_path in "$DOTFILES_DIR"/.[a-zA-Z]*; do
+        file="$(basename "$full_path")"
+        [[ -e "$full_path" ]] && 
+        [[ "$file" != ".config" ]] &&
+        [[ "$file" != ".local" ]] &&
+        [[ "$file" != .git* ]] &&
+        # Also check if it's symbolic link before deleting
+        [[ -L "$HOME/$file" ]] &&
+        rm -f "$HOME/$file"
+    done
+
+    # $XDG_CONFIG_HOME
+    for full_path in "$DOTFILES_DIR/.config"/[a-zA-Z]*; do
+        file="$(basename "$full_path")"
+        [[ -e "$full_path" ]] &&
+        [[ -L "$XDG_CONFIG_HOME/$file" ]] &&
+        rm -f "$XDG_CONFIG_HOME/$file"
+    done
+
+    # ~/.local/bin
+    full_path="$DOTFILES_DIR/.local/bin"
+    file="$(basename "$full_path")"
+    [[ -e "$full_path" ]] && [[ -L "$HOME/.local/$file" ]] && rm -f "$HOME/.local/$file"
+
+    # ~/.local/share ($XDG_DATA_HOME)
+    full_path="$DOTFILES_DIR/.local/share/riddle00"
+    file="$(basename "$full_path")"
+    [[ -e "$full_path" ]] && [[ -L "$XDG_DATA_HOME/$file" ]] && rm -f "$XDG_DATA_HOME/$file"
+
+    for full_path in "$DOTFILES_DIR/.local/share/applications"/[a-zA-Z]*; do
+        file="$(basename "$full_path")"
+        [[ -e "$full_path" ]] && [[ -L "$XDG_DATA_HOME/applications/$file" ]] && rm -f "$XDG_DATA_HOME/applications/$file"
+    done
+}
+
+# Passwordless sudo must be enabled
+apply_dotfiles() {
+    git clone -b "$repobranch" --depth 1 --recurse-submodules --shallow-submodules -j8 "$dotfilesrepo" "$HOME/.dotfiles"
+    sed -i "s/^export MAIN_USER=.*/export MAIN_USER=$username/" "$HOME/.dotfiles/.zshenv"
+    sudo chsh -s /bin/zsh "$username"
+    source "$HOME/.dotfiles/.zshenv"
+    symlink_dotfiles
+    prepare_sublime_text
+    make_some_files_immutable
+    install_vim_plugins
+    apply_finishing_touches
+}
+
+install_pkgs() {
+	[[ -f /etc/sudoers.pacnew ]] && cp /etc/sudoers.pacnew /etc/sudoers # just in case
+
+    # Use all cores for compilation (temporarily)
+	sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+
+    # Synchronizing system time to ensure successful and secure installation of software
+    ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
+
+    srcdir="/home/$username/.local/src"; mkdir -p "$srcdir"; sudo chown -R "$username":wheel $(dirname "$srcdir")
+
+    install_yay
+
+    # Updating the system before installing new software
+    pacman -Syu --noconfirm
+    runuser --pty -s /bin/bash -l "$username" -c "yay -Sua --noconfirm"
+
+    [[ -f "$progsfile" ]] || curl -LO "$progsfile"
+    sed '/^#/d' "$progsfile" > /tmp/progs.csv
+    while IFS=, read -r tag program comment; do
+        case "$tag" in
+            "A") install_from_aur "$program" ;;
+            "G") install_from_git "$program" ;;
+            *) install_from_main "$program" ;;
+        esac
+    done < /tmp/progs.csv
+
+    # Cleanup
+    rmdir "$srcdir" 2> /dev/null 
+
+	# Reset makepkg settings
+    sed -i "s/-j$(nproc)/-j2/;s/^MAKEFLAGS/#MAKEFLAGS/" /etc/makepkg.conf
+}
+
 # Install Arch from Live CD/USB
 install_arch() {
     # Check if Arch is to be installed on virtual machine or desktop computer
@@ -315,142 +443,14 @@ END
     reboot -f
 }
 
-install_pkgs() {
-	[[ -f /etc/sudoers.pacnew ]] && cp /etc/sudoers.pacnew /etc/sudoers # just in case
-
-    # Use all cores for compilation (temporarily)
-	sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
-
-    # Synchronizing system time to ensure successful and secure installation of software
-    ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
-
-    srcdir="/home/$username/.local/src"; mkdir -p "$srcdir"; sudo chown -R "$username":wheel $(dirname "$srcdir")
-
-    install_yay
-
-    # Updating the system before installing new software
-    pacman -Syu --noconfirm
-    runuser --pty -s /bin/bash -l "$username" -c "yay -Sua --noconfirm"
-
-    [[ -f "$progsfile" ]] || curl -LO "$progsfile"
-    sed '/^#/d' "$progsfile" > /tmp/progs.csv
-    while IFS=, read -r tag program comment; do
-        case "$tag" in
-            "A") install_from_aur "$program" ;;
-            "G") install_from_git "$program" ;;
-            *) install_from_main "$program" ;;
-        esac
-    done < /tmp/progs.csv
-
-    # Cleanup
-    rmdir "$srcdir" 2> /dev/null 
-
-	# Reset makepkg settings
-    sed -i "s/-j$(nproc)/-j2/;s/^MAKEFLAGS/#MAKEFLAGS/" /etc/makepkg.conf
-}
-
-# Passwordless sudo must be enabled
-apply_dotfiles() {
-    git clone -b "$repobranch" --depth 1 --recurse-submodules --shallow-submodules -j8 "$dotfilesrepo" "$HOME/.dotfiles"
-    sed -i "s/^export MAIN_USER=.*/export MAIN_USER=$username/" "$HOME/.dotfiles/.zshenv"
-    sudo chsh -s /bin/zsh "$username"
-    source "$HOME/.dotfiles/.zshenv"
-    symlink_dotfiles
-    prepare_sublime_text
-    make_some_files_immutable
-    install_vim_plugins
-    apply_finishing_touches
-}
-
-symlink_dotfiles() {
-    source "$HOME/.dotfiles/.zshenv"
-
-    [[ ! -d "$HOME/.config" ]] && mkdir -p "$HOME/.config"
-
-    [[ ! -d "$XDG_DATA_HOME/applications" ]] && mkdir -p "$XDG_DATA_HOME/applications"
-
-    # $HOME
-    for full_path in "$DOTFILES_DIR"/.[a-zA-Z]*; do
-        file="$(basename "$full_path")"
-        # Whenever you iterate over files/folders by globbing, it's good practice to avoid the corner 
-        # case where the glob does not match (which makes the loop variable expand to the 
-        # (un-matching) glob pattern string itself), hence [[ -e "$full_path" ] is used
-        [[ -e "$full_path" ]] && 
-            [[ "$file" != ".config" ]] &&
-            [[ "$file" != ".local" ]] &&
-            [[ "$file" != .git* ]] &&
-        ln -sf "$full_path" "$HOME/$file"
-    done
-
-    # $XDG_CONFIG_HOME
-    for full_path in "$DOTFILES_DIR/.config"/[a-zA-Z]*; do
-        file="$(basename "$full_path")"
-        [[ -e "$full_path" ]] &&
-        ln -sf "$full_path" "$XDG_CONFIG_HOME/$file"
-    done
-
-    # ~/.local/bin
-    local_bin="$DOTFILES_DIR/.local/bin"
-    [[ -e "$local_bin" ]] && ln -sf "$local_bin" "$HOME/.local/bin"
-
-    # ~/.local/share ($XDG_DATA_HOME)
-    local_shared="$DOTFILES_DIR/.local/share/riddle00"
-    [[ -e "$local_shared" ]] && ln -sf "$local_shared" "$XDG_DATA_HOME/riddle00"
-
-    for full_path in "$DOTFILES_DIR/.local/share/applications"/[a-zA-Z]*; do
-        file="$(basename "$full_path")"
-        [[ -e "$full_path" ]] &&
-        ln -sf $full_path "$XDG_DATA_HOME/applications/$file"
-    done
-}
-
-unlink_dotfiles() {
-    source "$HOME/.dotfiles/.zshenv"
-
-    # $HOME
-    for full_path in "$DOTFILES_DIR"/.[a-zA-Z]*; do
-        file="$(basename "$full_path")"
-        [[ -e "$full_path" ]] && 
-        [[ "$file" != ".config" ]] &&
-        [[ "$file" != ".local" ]] &&
-        [[ "$file" != .git* ]] &&
-        # Also check if it's symbolic link before deleting
-        [[ -L "$HOME/$file" ]] &&
-        rm -f "$HOME/$file"
-    done
-
-    # $XDG_CONFIG_HOME
-    for full_path in "$DOTFILES_DIR/.config"/[a-zA-Z]*; do
-        file="$(basename "$full_path")"
-        [[ -e "$full_path" ]] &&
-        [[ -L "$XDG_CONFIG_HOME/$file" ]] &&
-        rm -f "$XDG_CONFIG_HOME/$file"
-    done
-
-    # ~/.local/bin
-    full_path="$DOTFILES_DIR/.local/bin"
-    file="$(basename "$full_path")"
-    [[ -e "$full_path" ]] && [[ -L "$HOME/.local/$file" ]] && rm -f "$HOME/.local/$file"
-
-    # ~/.local/share ($XDG_DATA_HOME)
-    full_path="$DOTFILES_DIR/.local/share/riddle00"
-    file="$(basename "$full_path")"
-    [[ -e "$full_path" ]] && [[ -L "$XDG_DATA_HOME/$file" ]] && rm -f "$XDG_DATA_HOME/$file"
-
-    for full_path in "$DOTFILES_DIR/.local/share/applications"/[a-zA-Z]*; do
-        file="$(basename "$full_path")"
-        [[ -e "$full_path" ]] && [[ -L "$XDG_DATA_HOME/applications/$file" ]] && rm -f "$XDG_DATA_HOME/applications/$file"
-    done
-}
-
 # ========================== ENTRY POINT (EXECUTION) ==========================
 
 standalone_functions=(
-    "install_arch"
-    "install_pkgs"
-    "apply_dotfiles"
     "symlink_dotfiles"
     "unlink_dotfiles"
+    "apply_dotfiles"
+    "install_pkgs"
+    "install_arch"
 )
 
 while getopts ":f:m:u:b:p:h" opt; do 
@@ -463,11 +463,11 @@ while getopts ":f:m:u:b:p:h" opt; do
         h) printf "
         -f  [Required] Name of one single function to be run.
                 List of functions:
-                    'install_arch'
-                    'install_pkgs'
-                    'apply_dotfiles'
                     'symlink_dotfiles'
                     'unlink_dotfiles'
+                    'apply_dotfiles'
+                    'install_pkgs'
+                    'install_arch'
 
         -m  [Required only for the function 'install_arch'] Machine type. One of two values: {'vm', 'desktop'}
         -u  [Optional] User name. Defaults to 'userv'
